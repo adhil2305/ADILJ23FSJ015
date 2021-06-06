a9ce2d67c19048e500d2c2e08a98b210932095b5
adhil2305 <84714217+adhil2305@users.noreply.github.com>
2021-06-06 20:22:56 +0530

    Add files via upload

---
+++ CheckEvenOdd.java
@@ -0,0 +1,12 @@
+public class CheckEvenOdd {
+public static void main(Sting[] args) {
+int num = 20;
+/*if the number is divisible by 2 then
+*its an  even number
+*/
+if(num % == 2)
+System.out.println("The given number is even ");
+else
+System.out.println("The given number is odd");
+}
+ }
---
+++ SwapTwoNumbers.java
@@ -0,0 +1,22 @@
+public class SwapTwoNumbers {
+public static void main(String[] args) {
+ int first, second;
+ System.out.println("--Before Swap--");
+ System.out.println("first number= "+ first);
+ System.out.ptintln("Second number "+ second);
+ /**
+ *Value of 
+ *first number is assigned to Temporary Variable
+ */**
+ float temparary = first;
+ //Value of second is assigned to first
+ first = second;
+ //Value of temporary (Which contains the initial value of first is assigned )
+ is assigned to second
+second = temporary;
+System.out.println("--After Swap--");
+System.out.println("First Number =" + first);
+System.out.println("Second Number =" + second);
+
+}
+}
