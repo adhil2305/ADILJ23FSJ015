81f35715abe28152cd2769c5f6e98364c21053c7
adhil2305 <84714217+adhil2305@users.noreply.github.com>
2023-07-03 21:18:16 +0530

    Update CheckEvenOdd.java

--- CheckEvenOdd.java
+++ CheckEvenOdd.java
@@ -1,12 +1,1 @@
-public class CheckEvenOdd {
-public static void main(Sting[] args) {
-int num = 20;
-/*if the number is divisible by 2 then
-*its an  even number
-*/
-if(num % == 2)
-System.out.println("The given number is even ");
-else
-System.out.println("The given number is odd");
-}
- }
+
