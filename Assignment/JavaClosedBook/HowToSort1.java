68059b6443220491316ba2f359b01e17913204f5
adhil2305 <84714217+adhil2305@users.noreply.github.com>
2023-07-11 22:36:16 +0530

    Add files via upload

---
+++ Assignment/JavaClosedBook/HowToSort1.java
@@ -0,0 +1,33 @@
+package skirmish;
+
+public class HowToSort1 {
+	public static void main(String[] args) {
+		int arr[] = {4,6,1,7};
+		
+		for(int i=0;i<arr.length;i++){
+			for(int j=i+1;j<arr.length;j++) {
+				int temp = 0;
+				if(arr[j]<arr[i]) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+				
+			
+			}
+		System.out.print(arr[i] +" ");
+		
+		
+	}
+		System.out.println();
+		
+		for(int k=0;k<=4;k++) {
+			System.out.print(k+" k ");
+			for(int l=0;l<=4;l++) {
+				System.out.print(l + " l ");
+			}
+		}
+		
+	}
+
+}
